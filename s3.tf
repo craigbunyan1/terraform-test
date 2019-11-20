197ee7679f6a694ded74c9cd4d0e12f2372f5516
Craig Bunyan <craig.bunyan1@sainsburys.co.uk>
2019-11-20 12:00:59 +0000

    add terraform file with new s3 build and initial attempt at remote state

---
+++ s3.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+	region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "b3" {
+  bucket = "terraform-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My fourth bucket - ci test"
+    Environment = "Dev"
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-state-dev"
+    key    = "network/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}

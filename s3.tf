620a2938d8af2e3c4df096d95ddcf2622cc19e8a
Craig Bunyan <craig.bunyan1@sainsburys.co.uk>
2019-11-20 12:24:37 +0000

    change wrong syntac in tf file for tf state

--- s3.tf
+++ s3.tf
@@ -12,11 +12,9 @@ resource "aws_s3_bucket" "b3" {
   }
 }
 
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config = {
+terraform {
+  backend "s3" {
     bucket = "testing-test-tf-bucket-ci"
     key    = "network/terraform.tfstate"
-    region = "eu-west-2"
   }
 }
